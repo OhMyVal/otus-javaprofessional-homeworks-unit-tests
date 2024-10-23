6c33974ed0403cbad6b8cbfaa2328f2ef3fad3e9
ohmyval <makeup-style@mail.ru>
2024-10-23 22:58:36 +0300

    Test testAddAccount() done

--- src/test/java/ru/otus/bank/service/impl/AccountServiceImplTest.java
+++ src/test/java/ru/otus/bank/service/impl/AccountServiceImplTest.java
@@ -1,14 +1,17 @@
 package ru.otus.bank.service.impl;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
-import org.mockito.ArgumentMatcher;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.bank.dao.AccountDao;
+import ru.otus.bank.dao.AgreementDao;
 import ru.otus.bank.entity.Account;
+
+import ru.otus.bank.entity.Agreement;
 import ru.otus.bank.service.exception.AccountException;
 
 import java.math.BigDecimal;
@@ -25,6 +28,7 @@ public class AccountServiceImplTest {
     @Mock
     AccountDao accountDao;
 
+    private AgreementDao agreementDao = Mockito.mock(AgreementDao.class);
     @InjectMocks
     AccountServiceImpl accountServiceImpl;
 
@@ -82,5 +86,32 @@ public class AccountServiceImplTest {
 
         verify(accountDao).save(argThat(sourceMatcher));
         verify(accountDao).save(argThat(destinationMatcher));
-        }
+    }
+    @Test
+    public void testAddAccount() {
+
+        Agreement agreement = new Agreement();
+        agreement.setId(10L);
+
+        Account account = new Account();
+        String number = "accountNumber";
+        Integer type = 35;
+        BigDecimal amount = new BigDecimal(100);
+
+        account.setAgreementId(agreement.getId());
+        account.setNumber(number);
+        account.setType(type);
+        account.setAmount(amount);
+
+        ArgumentCaptor<Account> savedAccountsCaptor = ArgumentCaptor.captor();
+
+        Mockito.when(accountDao.save(savedAccountsCaptor.capture())).thenReturn(account);
+
+        Account result = accountServiceImpl.addAccount(agreement, number, type, amount);
+
+        Assertions.assertEquals(10L, result.getAgreementId());
+        Assertions.assertEquals("accountNumber", result.getNumber());
+        Assertions.assertEquals(35, result.getType());
+        Assertions.assertEquals(new BigDecimal(100), result.getAmount());
+    }
 }
